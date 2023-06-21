9e616b4fcdb472897dca679010013897318b7000
Andreapiani95 <andreapiani1995@gmail.com>
2023-06-21 17:17:59 +0200

    function, event complete

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
 document.getElementById('grid-generator').addEventListener('click',
+    function (){
+        createGrid(100);
+    }
    
-)
+);
 
+
+function createGrid(cellNumber){
+    for (i = 1; i <= cellNumber; i++){
+        const gridCell = document.createElement('div');
+        gridCell.append(i);
+        gridCell.classList.add('flex-cell');
+        document.querySelector('.grid').append(gridCell);
+    };
+}
     
